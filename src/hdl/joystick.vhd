1f15bb80988a12e4807363f8e842bb551b9e9ecf
Gaël Mariot <gael.mariot@gmail.com>
2025-03-10 18:26:28 +0100

    Working State Machine ... Just Read Now

--- src/hdl/joystick.vhd
+++ src/hdl/joystick.vhd
@@ -38,104 +38,127 @@ entity joystick is
   Port (
     -- Clock and Reset
     clk : in std_logic; 
-    reset : in std_logic;
+    nreset : in std_logic;
     -- cs defining if idling
-    cs: out std_logic;
-    -- Buffer send and receive --
-    tx : in std_logic_vector(39 downto 0);
-    rx : out std_logic_vector(39 downto 0)
+    ask : in std_logic;
+    -- joystick values --
+    always_on : out std_logic;
+    --x : out std_logic_vector(9 downto 0);
+    --y : out std_logic_vector(9 downto 0);
+    btn_trigger : out std_logic;
+    btn_joystick : out std_logic;
+    
+    leds : out STD_LOGIC_VECTOR(2 downto 0);
+    
+    --SPI INTERFACE -- 
+    sclk      : out STD_LOGIC; -- SPI clock
+    mosi      : out STD_LOGIC; -- Master Out Slave In (data to slave)
+    miso      : in  STD_LOGIC; -- Master In Slave Out (data from slave)
+    cs        : out STD_LOGIC  -- Chip Select (active low)
   );
 end joystick;
 
 -- Architecture body
 architecture Behavioral of joystick is
-    
-  -- SPI
-  signal MOSI : std_logic := '0';
-  signal MISO : std_logic := '0';
-  signal SCLK : std_logic := '0';
   
-  -- SPI communication signals
-  signal clk_div   : unsigned(15 downto 0) := (others => '0');  -- Clock divider
-  signal spi_clk   : std_logic := '0';      -- SPI clock signal
-  signal bit_count : integer := 0; -- Bit counter for SPI transfer
-  signal transfer  : std_logic := '0';      -- Signal to initiate SPI transfer
-  signal buffer_tx : std_logic_vector(39 downto 0) := (others => '0'); -- Transmit data buffer (5 bytes)
-  signal buffer_rx : std_logic_vector(39 downto 0) := (others => '0'); -- Receive data buffer (5 bytes)
+
+component spi_pmodjstk2_if is
+    generic (
+        DATA_WIDTH  : integer := 8; -- bits per SPI transaction
+        CLK_DIVIDER : integer := 10
+        );
+   Port (
+        clk       : in  STD_LOGIC; -- System clock
+        nrst       : in  STD_LOGIC; -- System reset
+        start     : in  STD_LOGIC; -- Start SPI transaction
+        data_in   : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0); -- Data to transmit
+        data_out  : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0); -- Data received
+        busy      : out STD_LOGIC; -- SPI master busy flag
+        sclk      : out STD_LOGIC; -- SPI clock
+        mosi      : out STD_LOGIC; -- Master Out Slave In (data to slave)
+        miso      : in  STD_LOGIC; -- Master In Slave Out (data from slave)
+        cs        : out STD_LOGIC;  -- Chip Select (active low)
+        state_out : out STD_LOGIC_VECTOR(2 downto 0)
+    );   
+end component spi_pmodjstk2_if;
+
+    signal joy_request : std_logic := '0';
+    signal joy_busy : std_logic := '0';
+    signal joy_data_in : std_logic_vector(39 downto 0) := (others=> '0');
+    signal joy_data_out: std_logic_vector(39 downto 0) := (others => '0');
+    signal ss : std_logic := '1';    
+    
+    signal count : integer := 0;
+
     
   -- Internal state machine
-  type state_type is (IDLE, SEND, RECEIVE, DONE);
+  type state_type is (IDLE,ASK_JOY,WAIT_JOY);
   signal state : state_type := IDLE;
-
 begin
-
-buffer_tx <= tx;
-rx <= buffer_rx;
-  -- SPI Clock Generation
-  process (clk, reset)
-  begin
-    if reset = '1' then
-      clk_div <= (others => '0');
-      spi_clk <= '0';
-    elsif rising_edge(clk) then
-      -- Go from 100MHz to 1MHz
-      if clk_div < 100 then
-        clk_div <= (others => '0');
-        spi_clk <= not spi_clk; -- Toggle SPI clock
-      else
-        clk_div <= clk_div + 1;
-      end if;
-    end if;
-  end process;
-
-  -- SPI Transaction State Machine
-  process (clk, reset)
-  begin
-    if reset = '1' then
-      cs <= '1';
-      MOSI <= '0';
-      bit_count <= 0;
-      state <= IDLE;
-      
-    elsif rising_edge(spi_clk) then
-      case state is
-        when IDLE =>
-          cs <= '1';  -- Deactivate cs
-          bit_count <= 0;
-          if transfer = '1' then
-            cs <= '0'; -- Activate cs
-            state <= SEND;
-          end if;
-
-        when SEND =>
-          -- Send data byte (MSB first)
-          if bit_count < 40 then
-            MOSI <= buffer_tx(39 - bit_count); -- Shift out MSB first
-            bit_count <= bit_count + 1;
-          else
-            bit_count <= 0;
-            state <= RECEIVE; -- Move to receive state
-          end if;
-
-        when RECEIVE =>
-          -- Receive byte from MISO
-          if bit_count < 40 then
-            buffer_rx(39 - bit_count) <= MISO; -- Shift in MSB first
-            bit_count <= bit_count + 1;
-          else
-            bit_count <= 0;
-            state <= DONE; -- Move to process state
-          end if;
-
-        when DONE =>
-          cs <= '1'; -- Deactivate cs
-          state <= IDLE; -- Return to IDLE state
-
-        when others =>
-          state <= IDLE;
-      end case;
-    end if;
-  end process;
+    
+always_on <= '1';
+joy : spi_pmodjstk2_if
+    generic map (
+      DATA_WIDTH => 40
+      )
+    port map(
+        clk         => clk  ,
+        nrst        => nreset  ,
+        start       => joy_request  ,
+        data_in     => joy_data_in ,
+        data_out    => joy_data_out  ,
+        busy        => joy_busy ,
+        sclk        => sclk ,
+        mosi        => mosi ,
+        miso        => miso ,
+        cs          => ss,
+        state_out => leds
+        );
+    cs <= ss;
+    
+    get_data: process(clk)
+        constant CMD_GET : std_logic_vector(39 downto 0) := (x"F000000000");
+    begin
+            
+        if(rising_edge(clk)) then
+            if(nreset = '0') then
+               -- x <= (others => '0');
+                --y <= (others => '0');
+                btn_trigger <= '0';
+                btn_joystick <= '0';
+                state <= IDLE;
+             else
+                    case state is
+                        when IDLE =>
+                            if(ask = '1') then
+                                state <= ASK_JOY;
+                            end if;
+                        when ASK_JOY =>
+                            joy_data_in <= CMD_GET;
+                            if(joy_busy = '0') then
+                                joy_request <= '1';
+                            elsif( joy_request = '1') then
+                                state<= WAIT_JOY;
+                            end if;
+                        
+                        when WAIT_JOY =>
+                            joy_request <= '0';
+                            if(joy_busy = '0') then
+                                --x <= joy_data_out(25) & joy_data_out(37) & joy_data_out(32);
+                                --Wy <= joy_data_out(9) & joy_data_out(19) & joy_data_out(16);
+                                --x <= joy_data_out(25 downto 24) & joy_data_out(39 downto 32);
+                                --y <= joy_data_out(9 downto 8) & joy_data_out(23 downto 16);
+                                btn_trigger <= joy_data_out(1);
+                                btn_joystick <= joy_data_out(0);
+                                
+                                state <= IDLE;
+                            end if;
+                        when others =>
+                            state <= IDLE;
+                    end case;             
+                end if;
+        end if;
+    end process get_data;
 
 
 end Behavioral;
--- src/hdl/spi_if.vhd
+++ src/hdl/spi_if.vhd
@@ -47,7 +47,8 @@ entity spi_pmodjstk2_if is
         sclk      : out STD_LOGIC; -- SPI clock
         mosi      : out STD_LOGIC; -- Master Out Slave In (data to slave)
         miso      : in  STD_LOGIC; -- Master In Slave Out (data from slave)
-        cs        : out STD_LOGIC  -- Chip Select (active low)
+        cs        : out STD_LOGIC;  -- Chip Select (active low)
+        state_out : out std_logic_vector(2 downto 0)
     );   
 end spi_pmodjstk2_if;
 
@@ -57,84 +58,94 @@ architecture Behavioral of spi_pmodjstk2_if is
     
     signal bit_counter : INTEGER := 0; -- Counter for bits transferred
     signal shift_reg   : STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0) := (others => '0'); -- Shift register for data
-    signal miso_reg    : STD_LOGIC := '0'; -- Register to store MISO input
     signal clk_cnt : integer := 0;
     signal sclk_bfr : std_logic := '0';
+    signal previous_sclk : std_logic := '0';
+    signal byte_counter : integer := 0;
 begin
-    sclk <= sclk_bfr;
     --SPI Clock
-    sclk_gen:process(clk,nrst)
+    sclk_gen:process(clk)
     begin
-        if(nrst = '0') then
-            sclk_bfr <= '0';
-        elsif(rising_edge(clk)) then
-            clk_cnt <= clk_cnt + 1;
-            if(clk_cnt = CLK_DIVIDER) then
-                clk_cnt <= 0;
-                sclk_bfr <= not sclk_bfr;
-            end if;
+        if(rising_edge(clk)) then
+                clk_cnt <= clk_cnt + 1;
+                previous_sclk <= sclk_bfr;
+                if(clk_cnt = CLK_DIVIDER) then
+                    clk_cnt <= 0;
+                    sclk_bfr <= not sclk_bfr;
+                end if;
         end if;
     end process sclk_gen;
     
     --SPI Transact
-    spi_transfer:process(sclk_bfr,nrst)
+    spi_transfer:process(clk)
     begin
-        if(nrst = '0') then
-            state <= IDLE;
-            cs <= '1'; -- Deactivate chip select
-            busy <= '0'; -- Not busy
-            bit_counter <= 0;
-            shift_reg <= (others => '0');
-            data_out <= (others => '0');
-            mosi <= '0';
-        elsif(rising_edge(sclk_bfr)) then
-            case state is
-                when IDLE =>
-                    cs <= '1'; -- Deactivate chip selects
-                    busy <= '0'; -- Not busy
-                    if start = '1' then
-                        cs <= '0'; -- Activate chip select
-                        busy <= '1'; -- Busy flag
-                        shift_reg <= data_in; -- Load input data into shift register
-                        bit_counter <= 0; -- Reset bit counter
-                        state <= TRANSFER;
-                    end if;
-                when WAIT_15us =>
-                    bit_counter <= bit_counter + 1;
-                    if(bit_counter > 15000/(10*CLK_DIVIDER)) then
-                        bit_counter <= 0;
-                        state <= TRANSFER;
-                    end if;
-                when WAIT_10us =>
-                    bit_counter <= bit_counter + 1;
-                    if(bit_counter > 10000/(10*CLK_DIVIDER)) then
-                        bit_counter <= 0;
-                        state <= TRANSFER;
-                    end if;
-                when WAIT_25us =>
-                    bit_counter <= bit_counter + 1;
-                    if(bit_counter > 25000/(10*CLK_DIVIDER)) then
-                        bit_counter <= 0;
+        if(rising_edge(clk)) then
+            if(nrst = '0') then
+                state <= IDLE;
+                cs <= '1'; -- Deactivate chip select
+                busy <= '0'; -- Not busy
+                bit_counter <= 0;
+                shift_reg <= (others => '0');
+                data_out <= (others => '0');
+                mosi <= '0';
+            elsif(previous_sclk = '0' and sclk_bfr = '1') then --Rising edge
+                case state is
+                    when IDLE =>
+                        state_out <= "000";
+                        cs <= '1'; -- Deactivate chip selects
                         busy <= '0'; -- Not busy
-                        state <= IDLE;
-                    end if;
-                when TRANSFER =>
-                    if bit_counter < DATA_WIDTH then
-                        -- Data transfer on rising or falling edge of SPI clock
-                        mosi <= shift_reg(DATA_WIDTH - 1); -- Output MSB of shift register
-                        shift_reg <= shift_reg(DATA_WIDTH - 2 downto 0) & miso; -- Shift left and insert MISO
-                        bit_counter <= bit_counter + 1; -- Increment bit counter
-                        if((bit_counter + 1) mod 8 = 0) then
-                            state <= WAIT_10us;
+                        if start = '1' then
+                            cs <= '0'; -- Activate chip select
+                            busy <= '1'; -- Busy flag
+                            shift_reg <= data_in; -- Load input data into shift register
+                            bit_counter <= 0; -- Reset bit counter
+                            state <= WAIT_15us;
+                            byte_counter <= 0;
                         end if;
-                    else
-                        bit_counter <= 0;
-                        state <= WAIT_25us;
-                        cs <= '1'; -- Deactivate chip select
-                    end if;
-                when others =>
-                    state <= IDLE;
-            end case;
+                    when WAIT_15us =>
+                        state_out <= "001";
+                        bit_counter <= bit_counter + 1;
+                        if(bit_counter > 15000/(10*CLK_DIVIDER)) then
+                            bit_counter <= 0;
+                            state <= TRANSFER;
+                        end if;
+                    when WAIT_10us =>
+                        state_out <= "010";
+                        bit_counter <= bit_counter + 1;
+                        if(bit_counter > 10000/(10*CLK_DIVIDER)) then
+                            bit_counter <= 0;
+                            state <= TRANSFER;
+                        end if;
+                    when WAIT_25us =>
+                        state_out <= "011";
+                        bit_counter <= bit_counter + 1;
+                        if(bit_counter > 25000/(10*CLK_DIVIDER)) then
+                            bit_counter <= 0;
+                            state <= IDLE;
+                        end if;
+                    when TRANSFER =>
+                        sclk <= sclk_bfr;
+                        state_out <= "100";
+                        if byte_counter < DATA_WIDTH/8 then
+                            -- Data transfer on rising or falling edge of SPI clock
+                            mosi <= shift_reg(DATA_WIDTH - 1); -- Output MSB of shift register
+                            shift_reg <= shift_reg(DATA_WIDTH - 2 downto 0) & miso; -- Shift left and insert MISO
+                            bit_counter <= bit_counter + 1; -- Increment bit counter
+                            if((bit_counter + 1) mod 8 = 0) then
+                                byte_counter <= byte_counter + 1;
+                                state <= WAIT_10us;
+                            end if;
+                        else
+                            byte_counter <= 0;
+                            bit_counter <= 0;
+                            state <= WAIT_25us;
+                            cs <= '1'; -- Deactivate chip select
+                        end if;
+                    when others =>
+                        state_out <= "111";
+                        state <= IDLE;
+                end case;
+            end if;
         end if;
     end process spi_transfer;
 
