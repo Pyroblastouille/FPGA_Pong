6eeadc8a3a324322f8074001aea5e61a4d5c7adb
Gaël Mariot <gael.mariot@gmail.com>
2025-03-17 18:35:49 +0100

    Working Joystick

--- src/constr/constr.xdc
+++ src/constr/constr.xdc
@@ -19,18 +19,18 @@ create_clock -period 10.000 -name sys_clk_pin -waveform {0.000 5.000} -add [get_
 
 
 ## LEDs
-set_property -dict {PACKAGE_PIN T14 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[0]}]
-set_property -dict {PACKAGE_PIN T15 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[1]}]
-set_property -dict {PACKAGE_PIN T16 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[2]}]
-set_property -dict {PACKAGE_PIN U16 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[3]}]
-set_property -dict {PACKAGE_PIN V15 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[4]}]
-set_property -dict {PACKAGE_PIN W16 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[5]}]
-set_property -dict {PACKAGE_PIN W15 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[6]}]
-set_property -dict {PACKAGE_PIN Y13 IOSTANDARD LVCMOS25} [get_ports {joy_1_y[7]}]
+#set_property -dict {PACKAGE_PIN T14 IOSTANDARD LVCMOS25} [get_ports btn_trigger]
+#set_property -dict {PACKAGE_PIN T15 IOSTANDARD LVCMOS25} [get_ports btn_joystick]
+#set_property -dict {PACKAGE_PIN T16 IOSTANDARD LVCMOS25} [get_ports always_on]
+#set_property -dict {PACKAGE_PIN U16 IOSTANDARD LVCMOS25} [get_ports {x[1]}]
+#set_property -dict {PACKAGE_PIN V15 IOSTANDARD LVCMOS25} [get_ports {x[0]}]
+#set_property -dict {PACKAGE_PIN W16 IOSTANDARD LVCMOS25} [get_ports {leds[2]}]
+#set_property -dict {PACKAGE_PIN W15 IOSTANDARD LVCMOS25} [get_ports {leds[1]}]
+#set_property -dict {PACKAGE_PIN Y13 IOSTANDARD LVCMOS25} [get_ports {leds[0]}]
 
 
 ## Buttons
-set_property -dict {PACKAGE_PIN B22 IOSTANDARD LVCMOS12} [get_ports reset]
+#set_property -dict {PACKAGE_PIN B22 IOSTANDARD LVCMOS12} [get_ports { ask }]
 #set_property -dict {PACKAGE_PIN D22 IOSTANDARD LVCMOS12} [get_ports start_btn]
 # set_property -dict { PACKAGE_PIN C22 IOSTANDARD LVCMOS12 } [get_ports { btnU }]; #IO_L20P_T3_16 Sch=btnl
 #set_property -dict { PACKAGE_PIN D14 IOSTANDARD LVCMOS12 } [get_ports { btnr }]; #IO_L6P_T0_16 Sch=btnr
@@ -39,14 +39,14 @@ set_property -dict {PACKAGE_PIN B22 IOSTANDARD LVCMOS12} [get_ports reset]
 
 
 ## Switches
-#set_property -dict { PACKAGE_PIN E22  IOSTANDARD LVCMOS12 } [get_ports { resetn }]; #IO_L22P_T3_16 Sch=sw[0]
+#set_property -dict { PACKAGE_PIN E22  IOSTANDARD LVCMOS12 } [get_ports { nreset }]; #IO_L22P_T3_16 Sch=sw[0]
 #set_property -dict { PACKAGE_PIN F21  IOSTANDARD LVCMOS12 } [get_ports { B }]; #IO_25_16 Sch=sw[1]
 #set_property -dict { PACKAGE_PIN G21  IOSTANDARD LVCMOS12 } [get_ports { C_in }]; #IO_L24P_T3_16 Sch=sw[2]
 #set_property -dict { PACKAGE_PIN G22  IOSTANDARD LVCMOS12 } [get_ports { sw[3] }]; #IO_L24N_T3_16 Sch=sw[3]
 #set_property -dict { PACKAGE_PIN H17  IOSTANDARD LVCMOS12 } [get_ports { sw[4] }]; #IO_L6P_T0_15 Sch=sw[4]
 #set_property -dict { PACKAGE_PIN J16  IOSTANDARD LVCMOS12 } [get_ports { sw[5] }]; #IO_0_15 Sch=sw[5]
-#set_property -dict { PACKAGE_PIN K13  IOSTANDARD LVCMOS12 } [get_ports { sw[6] }]; #IO_L19P_T3_A22_15 Sch=sw[6]
-#set_property -dict { PACKAGE_PIN M17  IOSTANDARD LVCMOS12 } [get_ports { sw[7] }]; #IO_25_15 Sch=sw[7]
+#set_property -dict {PACKAGE_PIN K13 IOSTANDARD LVCMOS12} [get_ports ask]
+set_property -dict {PACKAGE_PIN M17 IOSTANDARD LVCMOS12} [get_ports { reset }]
 
 
 ## OLED Display
@@ -106,10 +106,10 @@ set_property -dict {PACKAGE_PIN B22 IOSTANDARD LVCMOS12} [get_ports reset]
 
 
 ## Pmod header JA
-set_property -dict {PACKAGE_PIN AB22 IOSTANDARD LVCMOS33} [get_ports ss_1]
-set_property -dict {PACKAGE_PIN AB21 IOSTANDARD LVCMOS33} [get_ports mosi]
-set_property -dict {PACKAGE_PIN AB20 IOSTANDARD LVCMOS33} [get_ports miso]
-set_property -dict {PACKAGE_PIN AB18 IOSTANDARD LVCMOS33} [get_ports sclk_1]
+set_property -dict {PACKAGE_PIN AB22 IOSTANDARD LVCMOS33} [get_ports { j1_cs }]
+set_property -dict {PACKAGE_PIN AB21 IOSTANDARD LVCMOS33} [get_ports { j1_mosi }]
+set_property -dict {PACKAGE_PIN AB20 IOSTANDARD LVCMOS33} [get_ports { j1_miso }]
+set_property -dict {PACKAGE_PIN AB18 IOSTANDARD LVCMOS33} [get_ports { j1_sclk }]
 #set_property -dict { PACKAGE_PIN Y21   IOSTANDARD LVCMOS33 } [get_ports { ja[4] }]; #IO_L9P_T1_DQS_14 Sch=ja[7]
 #set_property -dict { PACKAGE_PIN AA21  IOSTANDARD LVCMOS33 } [get_ports { ja[5] }]; #IO_L8N_T1_D12_14 Sch=ja[8]
 #set_property -dict { PACKAGE_PIN AA20  IOSTANDARD LVCMOS33 } [get_ports { ja[6] }]; #IO_L8P_T1_D11_14 Sch=ja[9]
@@ -139,10 +139,10 @@ set_property -dict {PACKAGE_PIN AB18 IOSTANDARD LVCMOS33} [get_ports sclk_1]
 
 
 ## XADC Header
-#set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { xa_p[0] }]; #IO_L3P_T0_DQS_AD1P_15 Sch=xa_p[1]
-#set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { xa_n[0] }]; #IO_L3N_T0_DQS_AD1N_15 Sch=xa_n[1]
-#set_property -dict { PACKAGE_PIN H13   IOSTANDARD LVCMOS33 } [get_ports { xa_p[1] }]; #IO_L1P_T0_AD0P_15 Sch=xa_p[2]
-#set_property -dict { PACKAGE_PIN G13   IOSTANDARD LVCMOS33 } [get_ports { xa_n[1] }]; #IO_L1N_T0_AD0N_15 Sch=xa_n[2]
+set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { j2_cs }]; #IO_L3P_T0_DQS_AD1P_15 Sch=xa_p[1]
+set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { j2_mosi] }]; #IO_L3N_T0_DQS_AD1N_15 Sch=xa_n[1]
+set_property -dict { PACKAGE_PIN H13   IOSTANDARD LVCMOS33 } [get_ports { j2_miso }]; #IO_L1P_T0_AD0P_15 Sch=xa_p[2]
+set_property -dict { PACKAGE_PIN G13   IOSTANDARD LVCMOS33 } [get_ports { j2_sclk }]; #IO_L1N_T0_AD0N_15 Sch=xa_n[2]
 #set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { xa_p[2] }]; #IO_L2P_T0_AD8P_15 Sch=xa_p[3]
 #set_property -dict { PACKAGE_PIN G16   IOSTANDARD LVCMOS33 } [get_ports { xa_n[2] }]; #IO_L2N_T0_AD8N_15 Sch=xa_n[3]
 #set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { xa_p[3] }]; #IO_L5P_T0_AD9P_15 Sch=xa_p[4]
@@ -311,50 +311,3 @@ set_property -dict {PACKAGE_PIN AB18 IOSTANDARD LVCMOS33} [get_ports sclk_1]
 set_property CONFIG_VOLTAGE 3.3 [current_design]
 set_property CFGBVS VCCO [current_design]
 
-
-
-
-
-
-create_debug_core u_ila_0 ila
-set_property ALL_PROBE_SAME_MU true [get_debug_cores u_ila_0]
-set_property ALL_PROBE_SAME_MU_CNT 1 [get_debug_cores u_ila_0]
-set_property C_ADV_TRIGGER false [get_debug_cores u_ila_0]
-set_property C_DATA_DEPTH 1024 [get_debug_cores u_ila_0]
-set_property C_EN_STRG_QUAL false [get_debug_cores u_ila_0]
-set_property C_INPUT_PIPE_STAGES 0 [get_debug_cores u_ila_0]
-set_property C_TRIGIN_EN false [get_debug_cores u_ila_0]
-set_property C_TRIGOUT_EN false [get_debug_cores u_ila_0]
-set_property port_width 1 [get_debug_ports u_ila_0/clk]
-connect_debug_port u_ila_0/clk [get_nets [list mb_design_i/clk_wiz_0/inst/clk_100mhz]]
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe0]
-set_property port_width 2 [get_debug_ports u_ila_0/probe0]
-connect_debug_port u_ila_0/probe0 [get_nets [list {mb_design_i/JoystickGrabber_0/joy_1_x[8]} {mb_design_i/JoystickGrabber_0/joy_1_x[9]}]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe1]
-set_property port_width 1 [get_debug_ports u_ila_0/probe1]
-connect_debug_port u_ila_0/probe1 [get_nets [list mb_design_i/JoystickGrabber_0/joy_1_btn_trigger]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe2]
-set_property port_width 1 [get_debug_ports u_ila_0/probe2]
-connect_debug_port u_ila_0/probe2 [get_nets [list mb_design_i/JoystickGrabber_0/miso_1]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe3]
-set_property port_width 1 [get_debug_ports u_ila_0/probe3]
-connect_debug_port u_ila_0/probe3 [get_nets [list mb_design_i/JoystickGrabber_0/mosi_1]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe4]
-set_property port_width 1 [get_debug_ports u_ila_0/probe4]
-connect_debug_port u_ila_0/probe4 [get_nets [list mb_design_i/JoystickGrabber_0/s_axi_aclk]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe5]
-set_property port_width 1 [get_debug_ports u_ila_0/probe5]
-connect_debug_port u_ila_0/probe5 [get_nets [list mb_design_i/JoystickGrabber_0/sclk_1]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe6]
-set_property port_width 1 [get_debug_ports u_ila_0/probe6]
-connect_debug_port u_ila_0/probe6 [get_nets [list mb_design_i/JoystickGrabber_0/ss_1]]
-set_property C_CLK_INPUT_FREQ_HZ 300000000 [get_debug_cores dbg_hub]
-set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
-set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
-connect_debug_port dbg_hub/clk [get_nets u_ila_0_clk_100mhz]
--- src/hdl/JoystickGrabber.vhd
+++ src/hdl/JoystickGrabber.vhd
@@ -1,21 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 24.02.2025 16:10:54
--- Design Name: 
+-- Design Name:
 -- Module Name: JoystickGrabber - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
@@ -34,65 +34,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity JoystickGrabber is
 --  Port ( );
     generic(
-        C_AXI_ADDR_WIDTH : integer := 32;
-        C_AXI_DATA_WIDTH : integer := 32;
         C_ADDR_WIDTH : integer := 32;
         C_DATA_WIDTH : integer := 32);
     port(
     
     
-    -- ######################### --
-    -- AXI4-Lite INTERFACE       --
-    -- ######################### --
     s_axi_aclk      : in  std_logic;
     s_axi_aresetn   : in  std_logic;
-    -- AXI4-Lite Write interface
-    s_axi_awaddr    : in  std_logic_vector(C_AXI_ADDR_WIDTH-1 downto 0);
-    s_axi_awvalid   : in  std_logic;
-    s_axi_awready   : out std_logic;
-    s_axi_wdata     : in  std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-    s_axi_wstrb     : in  std_logic_vector(C_AXI_DATA_WIDTH/8-1 downto 0);
-    s_axi_wvalid    : in  std_logic;
-    s_axi_wready    : out std_logic;
-    s_axi_bresp     : out std_logic_vector(1 downto 0);
-    s_axi_bvalid    : out std_logic;
-    s_axi_bready    : in  std_logic;
-    -- AXI4-Lite Read interface
-    s_axi_araddr    : in  std_logic_vector(C_AXI_ADDR_WIDTH-1 downto 0);
-    s_axi_arvalid   : in  std_logic;
-    s_axi_arready   : out std_logic;
-    s_axi_rdata     : out std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-    s_axi_rresp     : out std_logic_vector(1 downto 0);
-    s_axi_rvalid    : out std_logic;
-    s_axi_rready    : in  std_logic;
-    
     -- ######################### --
     -- Joystick
     -- ######################### --
-    
-    joy_1_x       : out STD_LOGIC_VECTOR(9 downto 0); -- Joystick X position
-    joy_1_y       : out STD_LOGIC_VECTOR(9 downto 0); -- Joystick Y position
-    joy_1_btn_trigger : out STD_LOGIC; -- Trigger button state
-    joy_1_btn_joystick: out STD_LOGIC;  -- Joystick button state
-    
-    joy_2_x       : out STD_LOGIC_VECTOR(9 downto 0); -- Joystick X position
-    joy_2_y       : out STD_LOGIC_VECTOR(9 downto 0); -- Joystick Y position
-    joy_2_btn_trigger : out STD_LOGIC; -- Trigger button state
-    joy_2_btn_joystick: out STD_LOGIC;  -- Joystick button state
+    joy_1 : out std_logic_vector(C_ADDR_WIDTH -1 downto 0);
+    joy_2 : out std_logic_vector(C_ADDR_WIDTH -1 downto 0);
     
     -- ######################### --
     -- PMOD out
     -- ######################### --
-     
+
     miso_1 : in STD_LOGIC;
-    miso_2 : in STD_LOGIC;
     mosi_1 : out STD_LOGIC;
-    mosi_2 : out STD_LOGIC;
     sclk_1 : out STD_LOGIC;
-    sclk_2 : out STD_LOGIC;
     ss_1 : out STD_LOGIC;
+
+    miso_2 : in STD_LOGIC;
+    mosi_2 : out STD_LOGIC;
+    sclk_2 : out STD_LOGIC;
     ss_2 : out STD_LOGIC
-    
+
     );
 end JoystickGrabber;
 
@@ -100,221 +68,98 @@ architecture Behavioral of JoystickGrabber is
 
 
   ---------------------------------------------------------------------------------
-  -- AXI4-Lite interface
+  -- Joystick
   ---------------------------------------------------------------------------------
-  component axi4lite_if is
-    generic (
-      C_DATA_WIDTH : integer := 32;
-      C_ADDR_WIDTH : integer := 4
-      );
-    port (
-      s_axi_aclk    : in  std_logic;
-      s_axi_aresetn : in  std_logic;
-      s_axi_awaddr  : in  std_logic_vector(C_AXI_ADDR_WIDTH-1 downto 0);
-      s_axi_awvalid : in  std_logic;
-      s_axi_awready : out std_logic;
-      s_axi_wdata   : in  std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-      s_axi_wstrb   : in  std_logic_vector(C_AXI_DATA_WIDTH/8-1 downto 0);
-      s_axi_wvalid  : in  std_logic;
-      s_axi_wready  : out std_logic;
-      s_axi_bresp   : out std_logic_vector(1 downto 0);
-      s_axi_bvalid  : out std_logic;
-      s_axi_bready  : in  std_logic;
-      s_axi_araddr  : in  std_logic_vector(C_AXI_ADDR_WIDTH-1 downto 0);
-      s_axi_arvalid : in  std_logic;
-      s_axi_arready : out std_logic;
-      s_axi_rdata   : out std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-      s_axi_rresp   : out std_logic_vector(1 downto 0);
-      s_axi_rvalid  : out std_logic;
-      s_axi_rready  : in  std_logic;
-      wr_valid_o    : out std_logic;
-      wr_addr_o     : out std_logic_vector((C_ADDR_WIDTH - 1) downto 0);
-      wr_data_o     : out std_logic_vector((C_DATA_WIDTH - 1) downto 0);
-      rd_valid_o    : out std_logic;
-      rd_addr_o     : out std_logic_vector((C_ADDR_WIDTH - 1) downto 0);
-      rd_data_i     : in  std_logic_vector((C_DATA_WIDTH - 1) downto 0));
-  end component axi4lite_if;
-  
-  signal wr_valid_o    : std_logic;
-  signal wr_addr_o     : std_logic_vector((C_ADDR_WIDTH - 1) downto 0);
-  signal wr_data_o     : std_logic_vector((C_DATA_WIDTH - 1) downto 0);
-  signal rd_valid_o    : std_logic;
-  signal rd_addr_o     : std_logic_vector((C_ADDR_WIDTH - 1) downto 0);
-  signal rd_data_i     : std_logic_vector((C_DATA_WIDTH - 1) downto 0);
-  
-  ---------------------------------------------------------------------------------
-  -- Joystick 
-  ---------------------------------------------------------------------------------
-  
-  
 
-component spi_pmodjstk2_if is
-    generic (
-        DATA_WIDTH  : integer := 8; -- bits per SPI transaction
-        CLK_DIVIDER : integer := 10
-        );
-    
-   Port (
-        clk       : in  STD_LOGIC; -- System clock
-        nrst       : in  STD_LOGIC; -- System reset
-        start     : in  STD_LOGIC; -- Start SPI transaction
-        data_in   : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0); -- Data to transmit
-        data_out  : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0); -- Data received
-        busy      : out STD_LOGIC; -- SPI master busy flag
-        sclk      : out STD_LOGIC; -- SPI clock
-        mosi      : out STD_LOGIC; -- Master Out Slave In (data to slave)
-        miso      : in  STD_LOGIC; -- Master In Slave Out (data from slave)
-        cs        : out STD_LOGIC  -- Chip Select (active low)
-    );   
-end component spi_pmodjstk2_if;
 
-    signal joy_1_request: std_logic := '0';
-    signal joy_2_request: std_logic := '0';
-    signal joy_1_busy : std_logic := '0';
-    signal joy_2_busy : std_logic := '0';
-    
-    
-    signal joy_1_data_in : std_logic_vector(39 downto 0) := (others=> '0');
-    signal joy_1_data_out: std_logic_vector(39 downto 0) := (others => '0');
-    signal joy_2_data_in : std_logic_vector(39 downto 0) := (others=> '0');
-    signal joy_2_data_out: std_logic_vector(39 downto 0) := (others => '0');
-    
+  component pmod_joystick IS
+  GENERIC(
+    clk_freq        : INTEGER := 50); --system clock frequency in MHz
+  PORT(
+    clk             : IN     STD_LOGIC;                     --system clock
+    reset_n         : IN     STD_LOGIC;                     --active low reset
+    miso            : IN     STD_LOGIC;                     --SPI master in, slave out
+    mosi            : OUT    STD_LOGIC;                     --SPI master out, slave in
+    sclk            : BUFFER STD_LOGIC;                     --SPI clock
+    cs_n            : OUT    STD_LOGIC;                     --pmod chip select
+    x_position      : OUT    STD_LOGIC_VECTOR(7 DOWNTO 0);  --joystick x-axis position
+    y_position      : OUT    STD_LOGIC_VECTOR(7 DOWNTO 0);  --joystick y-axis position
+    trigger_button  : OUT    STD_LOGIC;                     --trigger button status
+    center_button   : OUT    STD_LOGIC);                    --center button status
+END component pmod_joystick;
+
+
   -- Internal state machine
   type state_type is (ASK_J1,WAIT_J1,ASK_J2,WAIT_J2);
   signal state : state_type := ASK_J1;
+  
+  --Joysticks
+  signal bfr_joy_1_x : std_logic_vector(7 downto 0);
+  signal bfr_joy_1_y : std_logic_vector(7 downto 0);
+  signal bfr_joy_1_btn_trigger : std_logic;
+  signal bfr_joy_1_btn_joystick : std_logic;
+
+  signal bfr_joy_2_x : std_logic_vector(7 downto 0);
+  signal bfr_joy_2_y : std_logic_vector(7 downto 0);
+  signal bfr_joy_2_btn_trigger : std_logic;
+  signal bfr_joy_2_btn_joystick : std_logic;
 begin
 
+
   ---------------------------------------------------------------------------------
-  -- AXI4-Lite interface
+  -- Joystick
   ---------------------------------------------------------------------------------
-  axi4lite_if_i : axi4lite_if
+  joystick_1 : pmod_joystick
     generic map (
-      C_DATA_WIDTH => C_DATA_WIDTH,
-      C_ADDR_WIDTH => C_ADDR_WIDTH
+      clk_freq => 100
       )
-    port map (
-      s_axi_aclk    => s_axi_aclk,
-      s_axi_aresetn => s_axi_aresetn,
-      s_axi_awaddr  => s_axi_awaddr,
-      s_axi_awvalid => s_axi_awvalid,
-      s_axi_awready => s_axi_awready,
-      s_axi_wdata   => s_axi_wdata,
-      s_axi_wstrb   => s_axi_wstrb,
-      s_axi_wvalid  => s_axi_wvalid,
-      s_axi_wready  => s_axi_wready,
-      s_axi_bresp   => s_axi_bresp,
-      s_axi_bvalid  => s_axi_bvalid,
-      s_axi_bready  => s_axi_bready,
-      s_axi_araddr  => s_axi_araddr,
-      s_axi_arvalid => s_axi_arvalid,
-      s_axi_arready => s_axi_arready,
-      s_axi_rdata   => s_axi_rdata,
-      s_axi_rresp   => s_axi_rresp,
-      s_axi_rvalid  => s_axi_rvalid,
-      s_axi_rready  => s_axi_rready,
-      wr_valid_o    => wr_valid_o,
-      wr_addr_o     => wr_addr_o,
-      wr_data_o     => wr_data_o,
-      rd_valid_o    => rd_valid_o,
-      rd_addr_o     => rd_addr_o,
-      rd_data_i     => rd_data_i
+    port map(
+        clk             => s_axi_aclk  ,
+        reset_n         => s_axi_aresetn  ,
+        miso            => miso_1  ,
+        mosi            => mosi_1 ,
+        sclk            => sclk_1  ,
+        cs_n            => ss_1 ,
+        x_position      => bfr_joy_1_x ,
+        y_position      => bfr_joy_1_y ,
+        trigger_button  => bfr_joy_1_btn_trigger ,
+        center_button   => bfr_joy_1_btn_joystick
+        );
+
+  joystick_2 : pmod_joystick
+  generic map (
+    clk_freq => 100
+    )
+  port map(
+      clk             => s_axi_aclk  ,
+      reset_n         => s_axi_aresetn  ,
+      miso            => miso_2  ,
+      mosi            => mosi_2 ,
+      sclk            => sclk_2  ,
+      cs_n            => ss_2 ,
+      x_position      => bfr_joy_2_x ,
+      y_position      => bfr_joy_2_y ,
+      trigger_button  => bfr_joy_2_btn_trigger ,
+      center_button   => bfr_joy_2_btn_joystick
       );
 
-
-  ---------------------------------------------------------------------------------
-  -- Joystick 
-  ---------------------------------------------------------------------------------
-  
-  joystick_1 : spi_pmodjstk2_if
-    generic map (
-      DATA_WIDTH => 40
-      )
-    port map(
-        clk         => s_axi_aclk  ,
-        nrst        => s_axi_aresetn  ,
-        start       => joy_1_request  ,
-        data_in     => joy_1_data_in ,
-        data_out    => joy_1_data_out  ,
-        busy        => joy_1_busy ,
-        sclk        => sclk_1 ,
-        mosi        => mosi_1 ,
-        miso        => miso_1 ,
-        cs          => ss_1
-        );
-        
-  joystick_2 : spi_pmodjstk2_if
-    generic map (
-      DATA_WIDTH => 40
-      )
-    port map(
-        clk         => s_axi_aclk  ,
-        nrst        => s_axi_aresetn  ,
-        start       => joy_2_request  ,
-        data_in     => joy_2_data_in ,
-        data_out    => joy_2_data_out  ,
-        busy        => joy_2_busy ,
-        sclk        => sclk_2 ,
-        mosi        => mosi_2 ,
-        miso        => miso_2 ,
-        cs          => ss_2
-        );
-        
     get_data: process(s_axi_aclk,s_axi_aresetn)
-        constant CMD_GET : std_logic_vector(39 downto 0) := (x"F000000000");
-    begin   
+    begin
         if(s_axi_aresetn = '0') then
-            joy_1_x <= (9 => '1',others => '0') ;
-            joy_1_y <= (9 => '1',others => '0') ;
-            joy_1_btn_trigger <= '0';
-            joy_1_btn_joystick<= '0';
-            joy_1_request <= '0';
-            
-            joy_2_x <= (9 => '1',others => '0') ;
-            joy_2_y <= (9 => '1',others => '0') ;
-            joy_2_btn_trigger <= '0';
-            joy_2_btn_joystick<= '0';
-            joy_2_request <= '0';
+            joy_1 <= (others => '0');
+            joy_2 <= (others => '0');
         elsif(rising_edge(s_axi_aclk)) then
-             case state is
-                when ASK_J1 =>
-                    joy_1_data_in <= CMD_GET;--cmd to send;
-                    if(joy_1_busy = '0') then
-                        joy_1_request <= '1';
-                        state <= WAIT_J1;
-                     end if;
-                when WAIT_J1 =>
-                    if (joy_1_busy = '0') then
-                        -- Extract Data --
-                        joy_1_x <= joy_1_data_out(25 downto 24) & joy_1_data_out(39 downto 32);
-                        joy_1_y <= joy_1_data_out(9 downto 8) & joy_1_data_out(23 downto 16);
-                        joy_1_btn_trigger <= joy_1_data_out(1);
-                        joy_1_btn_joystick <= joy_1_data_out(0); 
-                        -- Next State
-                        state <= ASK_J2;
-                    end if;
-                when ASK_J2 =>
-                    joy_2_data_in <= CMD_GET;--cmd to send;
-                    if(joy_2_busy = '0') then
-                        joy_2_request <= '1';
-                        state <= WAIT_J2;
-                     end if;
-                when WAIT_J2 =>
-                    if (joy_2_busy = '0') then
-                        -- Extract Data --
-                        joy_2_x <= joy_2_data_out(25 downto 24) & joy_2_data_out(39 downto 32);
-                        joy_2_y <= joy_2_data_out(9 downto 8) & joy_2_data_out(23 downto 16);
-                        joy_2_btn_trigger <= joy_2_data_out(1);
-                        joy_2_btn_joystick <= joy_2_data_out(0); 
-                        -- Next State
-                        state <= ASK_J1;
-                    end if;
-                when others =>
-                    state <= ASK_J1;
-            end case;          
-        end if;
+            joy_1(7 downto 0) <= bfr_joy_1_x(7 downto 0);
+            joy_1(15 downto 8) <= bfr_joy_1_y(7 downto 0);
+            joy_1(16) <= bfr_joy_1_btn_trigger;
+            joy_1(17) <= bfr_joy_1_btn_joystick;
             
-        
+            joy_2(7 downto 0) <= bfr_joy_2_x(7 downto 0);
+            joy_2(15 downto 8) <= bfr_joy_2_y(7 downto 0);
+            joy_2(16) <= bfr_joy_2_btn_trigger;
+            joy_2(17) <= bfr_joy_2_btn_joystick;
+        end if;
     end process get_data;
-        
-  
+
+
 end Behavioral;
